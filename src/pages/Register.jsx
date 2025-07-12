5c17d70e8bf04094074b87c4615195ac87213aa2
HKob25 <husseinakobeisi@gmail.com>
2025-07-12 16:15:34 +0300

    Broke down components, added input validation, added button functionality

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -10,15 +10,19 @@ import {LoginCard, SignUpCard} from '../pages/registerComponents/cards.jsx'
 
 const Register = () => { 
     const [isLogin, setLogin] = useState(false)
+    const login = () => {console.log("login")}
+    const signup = () => {console.log("signup")}
+
     
     return(
     <div className="main items-center fullscreen">
+
         <p className='title'>Nostalgia</p>
-        {isLogin ? <LoginCard setLogin={setLogin} register={login}/> : <SignUpCard setLogin={setLogin} register={signup}/>}
+
+        {isLogin ?  <LoginCard setLogin={setLogin} register={login}/> : 
+                    <SignUpCard setLogin={setLogin} register={signup}/>}
+
     </div>
 )}
-
-const login = () => {console.log("login")}
-const signup = () => {console.log("signup")}
 
 export default Register;
--- src/pages/registerComponents/components.jsx
+++
@@ -1,5 +0,0 @@
-export const Input = ({classesStr, placeholder }) => (
-  <input className={classesStr} placeholder={placeholder} />);
-
-export const Button = ({ text, onClick }) => (
-  <button className="cardBtn" onClick={onClick}>{text}</button>);
