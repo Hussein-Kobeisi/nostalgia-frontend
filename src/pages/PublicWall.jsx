64d3559652c2fe2e0492233068b08c143a08d9ca
HKob25 <husseinakobeisi@gmail.com>
2025-07-12 20:12:33 +0300

    Made initial design

--- src/pages/PublicWall.jsx
+++ src/pages/PublicWall.jsx
@@ -1,6 +1,25 @@
+import {useNavigate} from 'react-router-dom';
+import '../styles/common.css'
+import '../styles/PublicWall.css'
+
 const PublicWall = () => (
-    <div>
-        PublicWall
+    <div className="mainPage flex-col publicMain">
+        <div className='publicTextDiv flex-col items-center'>
+            <div className='bigPublicText'>Moments Shared by Others</div>
+            <div className='smallPublicText'>Dig deep and explore!</div>
+        </div>
+
+        <div className='publicYearDiv flex-col items-center'>
+            2020s
+        </div>
+        <>srg</>
+        <div className='publicYearDiv flex-col items-center'>
+            2015s
+        </div>
+
+        <div className='publicYearDiv flex-col items-center'>
+            2010s
+        </div>
     </div>
 )
 
