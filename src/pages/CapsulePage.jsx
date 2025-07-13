473e162b95e4ff3716591a9a6b2284278abcedc2
HKob25 <husseinakobeisi@gmail.com>
2025-07-13 19:28:17 +0300

    Added Header functionality

--- src/pages/CapsulePage.jsx
+++ src/pages/CapsulePage.jsx
@@ -3,7 +3,7 @@ import '../styles/CapsulePage.css';
 
 const CapsulePage = () => {
     const id = useParams().id;
-    const capsule = JSON.parse(localStorage.getItem("capsuleJsonData"))
+    const capsule = JSON.parse(localStorage.getItem("publicCapsuleJsonData"))
                     .find(capsule => capsule.id == id);
 
     capsule.texts.push("lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
